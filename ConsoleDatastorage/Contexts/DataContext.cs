e578478eb08acad7ec4f6b3a51b330c8b716025c
William <william.bjorklund.00@gmail.com>
2024-02-02 00:35:32 +0100

    Tables not added...

---
+++ ConsoleDatastorage/Contexts/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleDatastorage.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDatastorage.Contexts
+{
+    internal class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AdressEntity> Adresses { get; set; }
+        public DbSet<BrandEntity> Brands { get; set; }
+        public DbSet<CustomerEntity> Customers { get; set; }
+        public DbSet<ProductEntity> Products { get; set; }
+        public DbSet<ProductReviewEntity> Productreviews { get; set; }
+    }
+}
--- ConsoleDatastorage/Program.cs
+++ ConsoleDatastorage/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleDatastorage.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateDefaultBuilder().ConfigureServices(services =>
+{
+
+services.AddDbContext<DataContext>(x => x.UseSqlServer(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\willi\OneDrive\Skrivbord\Datastorage\ConsoleDatastorage\ConsoleDatastorage\Data\Database.mdf; Integrated Security = True; Connect Timeout = 30; Encrypt = True"));
+
+}).Build();
